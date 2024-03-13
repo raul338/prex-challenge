30701aa92463d5631fdf9e65e6d2c69fa9e07207
Raúl Arellano <rau.arellano92@gmail.com>
2024-03-13 20:23:30 -0300

    Add search endpoint

--- app/Http/Controllers/GifController.php
+++ app/Http/Controllers/GifController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SaveGifRequest;
+use App\Http\Requests\SearchGifRequest;
 use App\Models\Gif;
 use App\Models\User;
+use App\Services\GifService;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 
 class GifController extends Controller
 {
+    public function search(SearchGifRequest $request)
+    {
+        /** @var GifService $service */
+        $service = App::make(GifService::class);
+        $response = $service->search(
+            $request->validated('query'),
+            $request->validated('limit'),
+            $request->validated('offset')
+        );
+        return response()->json($response);
+    }
+
     public function save(User $user, SaveGifRequest $request)
     {
         return DB::transaction(function () use ($user, $request) {
--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/user/{user}/gif', [GifController::class, 'save'])->name('api.gif.save');
-});
+Route::middleware('auth:sanctum')
+    ->name('api.')
+    ->group(function () {
+        Route::get('/gif/search', [GifController::class, 'search'])->name('gif.search');
+        Route::put('/user/{user}/gif', [GifController::class, 'save'])->name('gif.save');
+    });
 
 Route::get('/user', function (Request $request) {
     return $request->user();
